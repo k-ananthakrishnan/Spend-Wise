9c7f79d29e4de6a753c1ee276d5763818fb3d098
ChristopherAntonyAJ <149337113+ChristopherAntonyAJ@users.noreply.github.com>
2024-08-02 17:13:28 -0400

    #10 added badges feature

--- script/category.js
+++ script/category.js
@@ -23,15 +23,15 @@ let selectedCategoryId = 0;
 
 function initializeFirestore() {
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD_kUpbp349u2AvKsbFLwbLwhnjTPCZndA",
-  authDomain: "mypwa-5fb18.firebaseapp.com",
-  projectId: "mypwa-5fb18",
-  storageBucket: "mypwa-5fb18.appspot.com",
-  messagingSenderId: "662572084401",
-  appId: "1:662572084401:web:dff7781d5ba43531fe0690",
-  measurementId: "G-DJPD4RB8BN"
-};
+    const firebaseConfig = {
+      apiKey: "AIzaSyD_kUpbp349u2AvKsbFLwbLwhnjTPCZndA",
+      authDomain: "mypwa-5fb18.firebaseapp.com",
+      projectId: "mypwa-5fb18",
+      storageBucket: "mypwa-5fb18.appspot.com",
+      messagingSenderId: "662572084401",
+      appId: "1:662572084401:web:dff7781d5ba43531fe0690",
+      measurementId: "G-DJPD4RB8BN"
+  };
   
     const app = initializeApp(firebaseConfig);
     return getFirestore(app);
